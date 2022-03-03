a0bbceab9f4e42b399c12ba99bcfdcccf9bf118a
Sanne de Vries <sannedv@protonmail.com>
2022-03-03 20:56:04 +0100

    Updated routes and fixed test

--- tests/acceptance/setup-test.js
+++ tests/acceptance/setup-test.js
@@ -1,8 +1,7 @@
-import moment from 'moment';
 import {Response} from 'ember-cli-mirage';
 import {authenticateSession, invalidateSession} from 'ember-simple-auth/test-support';
 import {beforeEach, describe, it} from 'mocha';
-import {blur, click, currentURL, fillIn, find, findAll} from '@ember/test-helpers';
+import {click, currentURL, fillIn, find, findAll} from '@ember/test-helpers';
 import {enableLabsFlag} from '../helpers/labs-flag';
 import {expect} from 'chai';
 import {setupApplicationTest} from 'ember-mocha';
